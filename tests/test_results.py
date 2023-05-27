70bdd3f5a167f2ff0015e64434c4352f704f927f
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-05-27 08:21:40 +0200

    Fixed tests with last version of sqlalchemy

--- tests/test_results.py
+++ tests/test_results.py
@@ -12,7 +12,7 @@ FILENAME = os.path.join(DIRNAME, 'queries/valid_queries.sql')
 def execute_query(url: str, query: str) -> t.List[t.Dict[str, t.Any]]:
     engine = sqlalchemy.create_engine(url + 'postgres')
     conn = engine.connect()
-    str_query = str(query).replace('%', '%%')
+    str_query = sqlalchemy.text(query)
     res = conn.execute(str_query)
     return [dict(row) for row in res.fetchall()]
 
