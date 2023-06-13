2b784e9d861bddd31c360992121a12d8c8297270
victoria de sainte agathe <vdsa@sarus.tech>
2023-06-13 22:25:45 +0200

    ok

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,8 @@ use qrlew_sarus::{
     data_spec,
     protobuf,
 };
+use serde_json::{from_str, Value};
+
 /*
 Error management
  */
@@ -67,22 +69,79 @@ struct Relation(Rc<relation::Relation>);
 
 #[pymethods]
 impl Relation {
+
+    fn parse(&self, query: &str, dataset: &Dataset) -> Result<Self> {
+        dataset.sql(query)
+    }
+
     fn __str__(&self) -> String {
         let relation = &*(self.0);
         format!("{}", relation)
     }
 
     fn dot(&self) -> Result<String> {
-        Ok((*self.0).dot()?)
+        let mut out: Vec<u8> = vec![];
+        (*self.0).dot(&mut out, &[]).unwrap();
+        Ok(String::from_utf8(out).unwrap())
+    }
+
+    fn schema(&self) -> Result<String> {
+        todo!()
+    }
+
+    fn protect(&self, dataset: &Dataset, protected_entity: &str) -> Result<Self> {
+        let relations = dataset.0.relations();
+        let pe = parse_protected_entity(protected_entity);
+        let pe_slice = pe.iter()
+            .map(
+                |(t, fk, n)| (
+                    t.as_str(),
+                    fk.iter().map(|(a1, a2, a3)| (a1.as_str(), a2.as_str(), a3.as_str())).collect::<Vec<(&str, &str, &str)>>().as_slice(),
+                    n.as_str()
+                )
+            )
+            .collect::<Vec<_>>()
+            .as_slice();
+        Ok(Relation(Rc::new((*(self.0)).clone().force_protect_from_field_paths(&relations, pe_slice))))
     }
 
     fn render(&self) -> String {
         let relation = &*(self.0);
         let ast_query: ast::Query = relation.into();
         format!("{}", ast_query)
-
     }
 
+}
+
+fn parse_protected_entity(str_pe: &str) -> Vec<(String, Vec<(String, String, String)>, String)>{
+    let v: Value = serde_json::from_str(str_pe)
+        .expect("Failed to parse JSON");
+    let mut peid: Vec<(String, Vec<(String, String, String)>, String)> = vec![];
+
+    if let Value::Array(json_array) = &v["protected_entity"] {
+        for table in json_array {
+            if let Value::Array(table_array) = &table {
+                assert_eq!(table_array.len(), 3);
+                let tablename = if let Value::String(s) = &table_array[0] {s.to_string()} else {panic!()};
+                let name = if let Value::String(s) = &table_array[2] {s.to_string()} else {panic!()};
+                let foreign_keys: Vec<(String, String, String)> = if let Value::Array(a) = &table_array[1] {
+                    a.iter()
+                    .map(|v| if let Value::Array(a) = &v {
+                        assert_eq!(a.len(), 3);
+                        let my_list = a.iter().map(|e| if let Value::String(s) = e {s.to_string()} else {panic!()}).collect::<Vec<String>>();
+                        (my_list[0].clone(), my_list[1].clone(), my_list[2].clone())
+                    } else {
+                        println!("v = {:?}", v); panic!()
+                    })
+                    .collect()
+                } else {
+                    panic!()
+                };
+                peid.push((tablename, foreign_keys, name))
+            } else {panic!()}
+        }
+        peid
+    } else {panic!()}
 }
 
 /// A Python module implemented in Rust.
@@ -91,4 +150,54 @@ fn pyqrlew(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Dataset>()?;
     m.add_class::<Relation>()?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_protected_entity() {
+        let str_pe = r#"
+        {
+            "protected_entity": [
+              ["rel1", [["col1", "rel2", "col2"], ["col3", "rel4", "col4"]], "coln"],
+              ["rel2", [["col1", "rel2", "col2"]], "coln"]
+            ]
+        }
+        "#;
+        let peid = parse_protected_entity(str_pe);
+        // let v: Value = serde_json::from_str(str_pe)
+        //     .expect("Failed to parse JSON");
+        // let mut peid: Vec<(&String, Vec<[&String; 3]>, &String)> = vec![];
+
+
+        // if let Value::Array(json_array) = &v["protected_entity"] {
+        //     for table in json_array {
+        //         if let Value::Array(table_array) = &table {
+        //             assert_eq!(table_array.len(), 3);
+        //             let tablename = if let Value::String(s) = &table_array[0] {s} else {panic!()};
+        //             let name = if let Value::String(s) = &table_array[2] {s} else {panic!()};
+
+
+        //             let foreign_keys: Vec<[&std::string::String; 3]> = if let Value::Array(a) = &table_array[1] {
+        //                 a.iter()
+        //                 .map(|v| if let Value::Array(a) = &v {
+        //                     assert_eq!(a.len(), 3);
+        //                     let my_list = a.iter().map(|e| if let Value::String(s) = e {s} else {panic!()}).collect::<Vec<&String>>();
+        //                     [my_list[0], my_list[1], my_list[2]]
+        //                 } else {
+        //                     println!("v = {:?}", v); panic!()
+        //                 })
+        //                 .collect()
+        //             } else {
+        //                 panic!()
+        //             };
+
+        //             peid.push((tablename, foreign_keys, name))
+        //         }
+        //     }
+        // }
+        println!("{:?}", peid);
+    }
 }
