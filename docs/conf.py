25ddff9ad3d1f96b19e20c61bad49938353d630d
Nicolas Grislain <Nicolas.Grislain@gmail.com>
2023-06-16 17:02:06 +0200

    Basic doc

--- docs/conf.py
+++ docs/conf.py
@@ -18,10 +18,19 @@ extensions = ['myst_parser']
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
+# MyST conf
+myst_enable_extensions = ["colon_fence"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
+
 html_static_path = ['_static']
+
+html_theme_options = {
+    'navbar_start': ['navbar-logo'],
+    'navbar_center': ['navbar-nav'],
+    'navbar_end': ['navbar-icon-links'],
+    'navbar_persistent': ['search-button']
+}
