4a8f1deb3501c9a00f20a1e56c1d63dd2e6a4398
victoria de sainte agathe <vdsa@sarus.tech>
2023-07-27 08:09:32 +0200

    ok

--- examples/simple.py
+++ examples/simple.py
@@ -7,10 +7,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 db = PostgreSQL()
 
-ds = db.extract()
-# ds = db.financial()
+# ds = db.extract()
+ds = db.financial()
 # ds = db.imdb()
 # ds = db.hepatitis()
+ds.relations()
 
 print(ds.relations())
-print(ds.sql("select * from census").dot())
+#print(ds.sql("select * from census").dot())
+print(ds.sql("select * from financial.account").dot())
