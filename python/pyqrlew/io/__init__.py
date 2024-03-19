3c7c28d1a6c43a432b32c95f004f63e201ac4871
Andi Cuko <andy.cuko@gmail.com>
2024-03-19 14:32:13 +0100

    fix ci and io init

--- python/pyqrlew/io/__init__.py
+++ python/pyqrlew/io/__init__.py
@@ -1,3 +1,14 @@
-from pyqrlew.io.postgresql import PostgreSQL
+import types
+import sys
+from pyqrlew.io.postgresql import PostgreSQL, dataset_from_database
 from pyqrlew.io.sqlite import SQLite
+
 from pyqrlew.io.utils import *
+
+# This is for compatibility with existing notebooks where we do:
+# from pyqrlew.io.dataset import dataset_from_database
+dataset = types.ModuleType('pyqrlew.io.dataset')
+# Populate the module with the desired function
+dataset.dataset_from_database = dataset_from_database
+# Add the module to sys.modules
+sys.modules['pyqrlew.io.dataset'] = dataset
