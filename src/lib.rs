8f90f3f54070ecfaaf5de7979931e5208ffa9449
Andi Cuko <andy.cuko@gmail.com>
2024-06-28 17:38:19 +0200

    add a hook for panic

--- src/lib.rs
+++ src/lib.rs
@@ -12,9 +12,23 @@ pub use crate::{
 use dp_event::RelationWithDpEvent;
 use pyo3::prelude::*;
 
+use std::panic;
+use backtrace::Backtrace;
+
+// Set a custom panic hook to print the backtrace
+fn set_panic_hook() {
+    panic::set_hook(Box::new(|info| {
+        eprintln!("panic occurred: {:?}", info);
+        let backtrace = Backtrace::new();
+        eprintln!("{:?}", backtrace);
+    }));
+}
+
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyqrlew(_py: Python, m: &PyModule) -> PyResult<()> {
+    set_panic_hook();
     m.add_class::<Dataset>()?;
     m.add_class::<Relation>()?;
     m.add_class::<Dialect>()?;
